75947831e6f16a12dd16410803a42cae2b2c5cc3
Angel Chakmaa <angelchakma@airs-MacBook-Air.local>
2026-08-13 23:52:35 +0600

    My project journey is going on

---
+++ Mad libs Genarator/Mad libs game.py
@@ -0,0 +1,12 @@
+
+
+User_name = input("Enter your name:")
+Father_name = input("Enter your father's name: ")
+Mother_name = input("Enter your mother's name: ")
+Age = input("Enter your age : ")
+Relationship = input("Enter your relationship status : ")
+
+Study = input("Enter your study field : ")
+print(f"Your  name is : {User_name}, and Your  Father's name is : {Father_name} and Your  mother's name is : {Mother_name}\n"
+      f"Your  age is : {Age} And Your  Relationship is : {Relationship}\n"
+      f"And she is Study in : {Study}\n")
