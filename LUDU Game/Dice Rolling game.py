c03ef85d86db048849dc2cc16677eec9f27de510
Angel Chakmaa <angelchakma@airs-MacBook-Air.local>
2026-08-25 13:10:12 +0600

    Finally my Dice rolling porject is completed

---
+++ LUDU Game/Dice Rolling game.py
@@ -0,0 +1,31 @@
+
+#This is Dice rolling game.
+
+import random
+Roll_number = random.randint(1,6)
+print("Welcome To our Dice Rolling game.")
+print(f"Your Roll number is : {Roll_number}.")
+
+while True:
+    Roll_number = random.randint(1,6)
+    
+    
+    User_input = input("Do you wnat To Role Again. [y/n]")
+    if User_input.lower().upper() == "Y":
+        print(f"Your Roll number is : {Roll_number}.")
+        continue
+
+    elif User_input.lower().upper() == "N":
+        print("The game is ended.")
+        break
+    else:
+        print("Invalid input try again.")
+
+
+#Finally the game is Completed by Angel Chakma.
+
+
+
+
+    
+
